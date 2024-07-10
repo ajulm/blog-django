09f7efbb98c67026005dc905cee061787307bc8f
Aljan011 <119142916+Aljan011@users.noreply.github.com>
2024-07-10 12:43:27 +0545

    Add files via upload

---
+++ myproject/templates/blog/models.py
@@ -0,0 +1,25 @@
+import uuid
+from django.db import models
+from ckeditor.fields import RichTextField
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    featured_image = models.ImageField(upload_to='featured_images/')
+    slug = models.SlugField(unique=True)
+    content = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            if self.title:
+                self.slug = slugify(self.title)
+            else:
+                self.slug = uuid.uuid4().hex[:8]  
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title or str(self.id) 
